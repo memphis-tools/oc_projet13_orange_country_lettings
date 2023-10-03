5105cbdf9bb8233a883d11b75bf77cf1e721375a
memphis-tools <rn@github.com>
2023-10-03 03:46:30 +0200

    Premiere itération de tests. Fixture créée en 'conftest.py', mise à jour setup.cfg.

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,66 @@
-def test_dummy():
-    assert 1
+import pytest
+from django.test.client import RequestFactory
+from django.contrib.auth.models import User
+from django.conf.urls import handler404, handler500
+
+from django.urls import reverse, resolve
+from django.test import Client
+
+
+@pytest.mark.django_db
+def test_index_url():
+    client = Client()
+    path = reverse('index', kwargs={})
+    response = client.get(path)
+    assert path == "/"
+    assert resolve(path).view_name == "index"
+
+
+@pytest.mark.django_db
+def test_lettings_url():
+    client = Client()
+    path = reverse('lettings_index', kwargs={})
+    response = client.get(path)
+    assert path == "/lettings/"
+    assert response.status_code == 200
+    assert resolve(path).view_name == "lettings_index"
+
+
+@pytest.mark.django_db
+def test_lettings_detail_url(get_dummy_db):
+    client = Client()
+    path = reverse('letting', kwargs={'letting_id':1})
+    response = client.get(path)
+    assert path == "/lettings/1/"
+    assert response.status_code == 200
+    assert resolve(path).view_name == "letting"
+
+
+@pytest.mark.django_db
+def test_profiles_url():
+    client = Client()
+    path = reverse('profiles_index', kwargs={})
+    response = client.get(path)
+    assert path == "/profiles/"
+    assert response.status_code == 200
+    assert resolve(path).view_name == "profiles_index"
+
+
+@pytest.mark.django_db
+def test_profiles_detail_url(get_dummy_db):
+    client = Client()
+    user = User.objects.get(id=1)
+    path = reverse('profile', kwargs={'username':user.username})
+    response = client.get(path)
+    assert path == f"/profiles/{user.username}/"
+    assert response.status_code == 200
+    assert resolve(path).view_name == "profile"
+
+
+@pytest.mark.django_db
+def test_dummy_404_url():
+    client = Client()
+    factory = RequestFactory()
+    request = factory.get('/farine/ble')
+    response = handler404(request, "")
+    assert response.status_code == 404
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,2 +1,12 @@
-def test_dummy():
-    assert 1
+import pytest
+
+from django.test import Client
+from profiles.models import Profile
+
+
+@pytest.mark.django_db
+def test_profile_model(get_dummy_db):
+    client = Client()
+    profile = Profile.objects.get(id=1)
+    expected_value = "jrodriguez"
+    assert str(profile) == expected_value
