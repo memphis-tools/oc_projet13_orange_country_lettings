17d4545844d0a12a904d591beac653d8a20264a0
memphis-tools <rn@github.com>
2023-10-04 16:52:14 +0200

    forcer en dur checmin staticfiles

--- oc_projet13/oc_lettings_site/settings.py
+++ oc_projet13/oc_lettings_site/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = "/opt/render/project/src/oc_projet13/staticfiles/"
+STATIC_ROOT = "staticfiles/"
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = ["./static"]
 
 # WHITENOISE_MANIFEST_STRICT = False
 # if not DEBUG:
