bee5afa951e0308f5dc3cdb0289b4c5d0bcb3d19
memphis-tools <rn@github.com>
2023-10-04 15:18:24 +0200

    WHITENOISE_MANIFEST_STRICT mis à False

--- oc_projet13/oc_lettings_site/settings.py
+++ oc_projet13/oc_lettings_site/settings.py
@@ -119,5 +119,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+WHITENOISE_MANIFEST_STRICT = False
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
