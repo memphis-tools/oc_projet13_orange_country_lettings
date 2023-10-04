516e5b1beef2c4dfccc32503d2cf2501df641689
memphis-tools <rn@github.com>
2023-10-04 06:04:28 +0200

    Render. On remet la déclaration STATIC_ROOT en dehors du test 'if DEBUG'

--- oc_projet13/oc_lettings_site/settings.py
+++ oc_projet13/oc_lettings_site/settings.py
@@ -115,7 +115,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
