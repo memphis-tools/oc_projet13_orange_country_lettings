2075441bb1e774e6ea0bb1a8abf5668e316de0b2
memphis-tools <rn@github.com>
2023-10-04 05:59:09 +0200

    Render. Mise à jour fichier settings de Django.

--- oc_projet13/entrypoint.sh
+++ oc_projet13/entrypoint.sh
@@ -5,5 +5,5 @@
 # python ./manage.py makemigrations profiles --no-input
 # python ./manage.py migrate --no-input
 # python ./manage.py collectstatic --no-input --clear
-#
+
 # gunicorn oc_lettings_site.wsgi:application --bind 0.0.0.0:8000
