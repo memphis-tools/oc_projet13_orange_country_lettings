df958b65eac4406f60ebee139a7f63851c135ff3
memphis-tools <rn@github.com>
2023-10-19 00:23:59 +0200

    mise a jour check_lint.py

--- check_lint.py
+++ check_lint.py
@@ -1,9 +1,9 @@
+import os
 import re
-import logtail_handler
+import requests
 import socket
 
 
-LOGGER = logtail_handler.logger
 LOCAL_IP = socket.gethostbyname(socket.gethostname())
 PATTERN = 'id="all-good"'
 DATAS = ''
@@ -16,4 +16,11 @@ is_good = re.search(PATTERN, str(DATAS))
 
 if not is_good.group():
     message = f"{LOCAL_IP} Warning - Lint has to be done"
-    LOGGER.error(message)
+    url = 'https://in.logs.betterstack.com'
+    betterstack_token = os.getenv('BETTERSTACK_SOURCE_TOKEN')
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": f"Bearer {betterstack_token}"
+    }
+    body = {'message': message}
+    requests.post(url, json = body)
